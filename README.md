4820d21b46fc194ce28d1b5a8ddcf49f2978b3e7
Evania <evan.nhea@gmail.com>
2018-07-12 15:47:00 +0700

    add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# SLIce HTML Page
+
+## Cara Instalasi
+1. Jalankan git clone `https://github.com/nhunhea/slice.git`
+2. Jalankan npm install
+3. Jalankan gulp build
+4. Jalankan gulp server
--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,29 @@
 const gulp = require('gulp');
-var gulpMinifyCss = require('gulp-minify-css');
-var gulpConcat = require('gulp-concat');
-var gulpUglify = require('gulp-uglify');
-var gulpHtmlmin = require('gulp-htmlmin');
-var gulpConnect = require('gulp-connect');
+const gulpMinifyCss = require('gulp-minify-css');
+const gulpConcat = require('gulp-concat');
+const gulpUglify = require('gulp-uglify');
+const gulpHtmlmin = require('gulp-htmlmin');
+const gulpConnect = require('gulp-connect');
+const gulpImagemin = require('gulp-imagemin');
+const gulpSequence = require('gulp-sequence');
 
 gulp.task('minify-css', function() {
   gulp.src('./src/css/style.css')
     .pipe(gulpMinifyCss({
       compatibility: 'ie8'
     }))
-    .pipe(gulp.dest('./build/'))
+    .pipe(gulp.dest('build/src/css'))
     .pipe(gulpConnect.reload());
 });
 
 gulp.task('minify-js', function() {
   gulp
     .src([
-      './src/javascript/slider.js',
-      './src/javascript/slider2.js',
+      './src/javascript/slider.js'
     ])
-    .pipe(gulpConcat('bundle.js'))
+    .pipe(gulpConcat('slider.js'))
     .pipe(gulpUglify())
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest('build/src/javascript'))
     .pipe(gulpConnect.reload());
 });
 
@@ -31,9 +32,18 @@ gulp.task('minify-html', function() {
     .pipe(gulpHtmlmin({
       collapseWhitespace: true
     }))
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest('build/'))
     .pipe(gulpConnect.reload());
 });
+
+gulp.task('imagemin', function() {
+  gulp
+    .src('./assets/*')
+    .pipe(gulpImagemin())
+    .pipe(gulp.dest('./build/assets/'))
+ });
+
+gulp.task('build', gulpSequence('minify-css', 'minify-js', 'minify-html', 'imagemin'));
 
 gulp.task('server', function() {
   gulpConnect.server({
